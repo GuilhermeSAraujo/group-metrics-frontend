94c8c274465b60ae2489c0dfe9eaa0bb7cfa62e9
Guilherme Souza <guilhermesouzae21@gmail.com>
2026-05-10 23:25:18 -0300

    Update default API base URL to new Cloudflare endpoint

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,4 @@
-const DEFAULT_BASE = 'http://188.245.83.65:3030'
+const DEFAULT_BASE = 'https://goal-bottom-frame-alice.trycloudflare.com'
 
 /**
  * @param {string} [baseUrl] - API origin (no trailing slash). Override with VITE_API_BASE_URL.
